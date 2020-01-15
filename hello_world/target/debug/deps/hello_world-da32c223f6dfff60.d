b77e9e49695b55ce2cda29ee801977abbc2cfec2
Mridu Nanda <mridu.nanda@gmail.com>
2020-01-14 19:53:10 -0500

    hello world

---
+++ hello_world/target/debug/deps/hello_world-da32c223f6dfff60.d
@@ -0,0 +1,5 @@
+/Users/mridunanda/Learning-Rust/hello_world/target/debug/deps/hello_world-da32c223f6dfff60: src/main.rs
+
+/Users/mridunanda/Learning-Rust/hello_world/target/debug/deps/hello_world-da32c223f6dfff60.d: src/main.rs
+
+src/main.rs:
